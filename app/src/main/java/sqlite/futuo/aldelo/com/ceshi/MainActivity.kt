851b2d00a103320027935935e65b011ffec35754
futuo <fu.tuo@aldelo.com>
2020-02-22 11:04:29 +0800

    Initial commit

---
+++ app/src/main/java/sqlite/futuo/aldelo/com/ceshi/MainActivity.kt
@@ -0,0 +1,12 @@
+package sqlite.futuo.aldelo.com.ceshi
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
